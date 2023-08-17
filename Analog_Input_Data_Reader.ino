6c27ef14dfa71542f5aa08b909bdc6c0026ca210
Yuvraj9021 <114735630+Yuvraj9021@users.noreply.github.com>
2023-08-17 12:55:35 +1000

    Add files via upload

---
+++ Analog_Input_Data_Reader.ino
@@ -0,0 +1,17 @@
+//
+const int potPin = 4;
+
+int potValue = 0;
+
+void setup() {
+Serial.begin(9600);
+delay(1000);
+
+}
+
+void loop(){
+  potValue = analogRead(potPin);
+  Serial.println(potValue);
+  delay(500);
+
+}
