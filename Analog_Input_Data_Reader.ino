313cd722edc8b0730d313f550f6ea9a19087f24b
Yuvraj9021 <114735630+Yuvraj9021@users.noreply.github.com>
2023-08-20 20:29:51 +1000

    Analog_Input_Data_Reader.ino

--- Analog_Input_Data_Reader.ino
+++ Analog_Input_Data_Reader.ino
@@ -1,17 +1,22 @@
 //
 const int potPin = 4;
 
+// Variable to store potentiometer value
 int potValue = 0;
 
 void setup() {
-Serial.begin(9600);
-delay(1000);
 
+Serial.begin(9600); // Initialize serial communication for debugging
+
+delay(1000); // Delay for 1 second to allow setup time
 }
 
-void loop(){
-  potValue = analogRead(potPin);
-  Serial.println(potValue);
-  delay(500);
+void loop() {
+
+potValue = analogRead(potPin); // Read the analog value from the potentiometer
+
+Serial.println(potValue); // Print the potentiometer value to the serial monitor
+
+delay(500); // A delay to control the rate of output
 
 }
